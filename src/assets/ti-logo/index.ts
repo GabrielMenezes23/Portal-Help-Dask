fb94f7efaa52f34c059b9802fbf6847b074734c8
GABRIEL MENEZES <gabriel7.ieq64@gmail.com>
2026-08-04 18:50:28 -0300

    fix: remove failed embedded logo chunks

--- src/assets/ti-logo/index.ts
+++
@@ -1,6 +0,0 @@
-import part00 from './part-00';
-import part01 from './part-01';
-import part02 from './part-02';
-import part03 from './part-03';
-
-export const TI_LOGO_DATA_URI = `data:image/webp;base64,${part00}${part01}${part02}${part03}`;
